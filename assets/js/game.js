972c8f4f8e8cf1e07c9a92b33206bd003075596b
KyleKilmartin371 <kylekilmartin@yahoo.com>
2021-06-26 17:07:35 -0700

    added the first functions to game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robots name?")
+console.log(playerName)
+console.log("This log is a string, good for leaving yourself a message")
+//this will do math and log 20
+console.log(10+10);
+//What is this?
+console.log("Our robot's name is " + playerName);
+function fight() {
+    window.alert("The fight has begun!");
+  }
+//fight();
