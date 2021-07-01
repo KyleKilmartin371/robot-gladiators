e87020fe53d7188268fcdcc501b20d3c403e052e
KyleKilmartin371 <kylekilmartin@yahoo.com>
2021-06-30 17:29:44 -0700

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -71,6 +71,13 @@ var fight = function(enemyName) {
     }
  }
 };
+//function to start a new game
+var startGame = function(){
+    //reset players stays
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
 for(var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
         window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ));
@@ -84,3 +91,30 @@ else{
     break;
     }
 }
+
+     //after the loop ends, player is either out of health or enemies to battle, so run the endGame function
+     endGame();
+};
+
+    var endGame = function(){
+        //if player is still alive, player wins!
+        if (playerHealth > 0) {
+            window.alert("Great job, you've survived the game! You now have a score of " + playerMoney +  ".");
+        }
+        else{
+            window.alert("You've lost your robot in battle.");
+        }
+    
+        //ask player if they'd like to play again
+        var playAgainConfirm = window.confirm("Would yoou like to play again?");
+    
+        if (playAgainConfirm) {
+            //restart the game
+            startGame();
+        }
+        else{
+            window.alert("Thank you for playing Robot Galdiators! Come back soon!");
+        }
+    };
+//start the game when the page loads
+startGame();
